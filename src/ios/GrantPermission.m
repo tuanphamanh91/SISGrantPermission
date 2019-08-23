5dd5b07d780139ec08edceeea7abaabebc2b0001
TuanPA <tuanpa@sis-vn.com>
2019-08-23 09:30:10 +0700

    update latest code for plugin

--- src/ios/GrantPermission.m
+++ src/ios/GrantPermission.m
@@ -14,58 +14,77 @@
 
 - (void)grantNotificationPermission:(CDVInvokedUrlCommand*)command
 {
-    [self.commandDelegate runInBackground:^{
-        // do work here
-        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    // [self.commandDelegate runInBackground:^{
+    //     // do work here
+        
+    // }];
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
         [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error){
             NSLog(@"grant notification");
+            if (granted) {
+                [[UIApplication sharedApplication] registerForRemoteNotifications];
+            }
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            
         }];
-    }];
-    
     
 }
 
 - (void)grantCameraPermission:(CDVInvokedUrlCommand*)command
 {
-    [self.commandDelegate runInBackground:^{
-        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-        if(status == AVAuthorizationStatusNotDetermined){ // not determined
-            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
-                //
-                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            }];
-        } else {
+//    [self.commandDelegate runInBackground:^{
+//        
+//    }];
+    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    if(status == AVAuthorizationStatusNotDetermined){ // not determined
+        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+            //
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        }
-    }];   
+        }];
+    } else {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
 }
 
 - (void)isGrantPermission:(CDVInvokedUrlCommand*)command {
-    [self.commandDelegate runInBackground:^{
-        // do work here
-        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-        [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error){
-            NSLog(@"grant notification");
-            if (granted) {
-                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            } else {
-                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:true];
+    [[UNUserNotificationCenter currentNotificationCenter] getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+        switch (settings.authorizationStatus) {
+                // This means we have not yet asked for notification permissions
+            case UNAuthorizationStatusNotDetermined:
+            {
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:false];
                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
             }
-            
-            
-        }];
+                break;
+                // We are already authorized, so no need to ask
+            case UNAuthorizationStatusAuthorized:
+            {
+                // Just try and register for remote notifications
+                NSLog(@"CDVCommandStatus_OK");
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:true];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            }
+                break;
+                // We are denied User Notifications
+            case UNAuthorizationStatusDenied:
+            {
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:true];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        
+            }
+                break;
+            case UNAuthorizationStatusProvisional: {
+                
+                break;
+            }
+        }
+        
     }];
 }
 
 -(void)openSetting:(CDVInvokedUrlCommand*)command {
-    NSLog(@"openSetting");
     BOOL canOpenSettings = (&UIApplicationOpenSettingsURLString != NULL);
     if (canOpenSettings) {
         NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
--- www/permission.js
+++ www/permission.js
@@ -1,30 +1,29 @@
-var cordova = require('cordova');
-
-// Empty constructor
-function GrantPermissionsFunction() {}
-
-GrantPermissionsFunction.prototype.grantNotificationPermission = function (text, success, error) {
-    cordova.exec(success, error, 'meomeopermission', 'grantNotificationPermission', [text]);
-};
-
-GrantPermissionsFunction.prototype.grantCameraPermission = function (arg0, success, error) {
-    cordova.exec(success, error, 'meomeopermission', 'grantCameraPermission', [arg0]);
-};
-
-GrantPermissionsFunction.prototype.openSetting = function (arg0, success, error) {
-    console.log("isGrantPermission");
-    console.log('====================================');
-    cordova.exec(success, error, 'meomeopermission', 'openSetting', [arg0]);
-};
-
-GrantPermissionsFunction.prototype.isGrantPermission = function (arg0, success, error) {
-    console.log("isGrantPermission");
-    console.log('====================================');
-    cordova.exec(success, error, 'meomeopermission', 'isGrantPermission', [arg0]);
-};
-
-
-
-// Register the plugin
-var grantPermissionsFunc = new GrantPermissionsFunction();
-module.exports = grantPermissionsFunc;
+cordova.define("cordova-plugin-ios-grant-permission.permission", function(require, exports, module) {
+    var cordova = require('cordova');
+    
+    // Empty constructor
+    function GrantPermissionsFunction() {}
+    
+    GrantPermissionsFunction.prototype.grantNotificationPermission = function (text, success, error) {
+        cordova.exec(success, error, 'meomeopermission', 'grantNotificationPermission', [text]);
+    };
+    
+    GrantPermissionsFunction.prototype.grantCameraPermission = function (arg0, success, error) {
+        cordova.exec(success, error, 'meomeopermission', 'grantCameraPermission', [arg0]);
+    };
+    
+    GrantPermissionsFunction.prototype.openSetting = function (arg0, success, error) {
+        cordova.exec(success, error, 'meomeopermission', 'openSetting', [arg0]);
+    };
+    
+    GrantPermissionsFunction.prototype.isGrantPermission = function (arg0, success, error) {
+        cordova.exec(success, error, 'meomeopermission', 'isGrantPermission', [arg0]);
+    };
+    
+    
+    
+    // Register the plugin
+    var grantPermissionsFunc = new GrantPermissionsFunction();
+    module.exports = grantPermissionsFunc;
+    });
+    
